76c56d719733c0e867d2d6c66dd336067ced183e
2260784938 <2260784938@qq.com>
2020-04-22 23:26:29 +0800

    删除无用import

--- src/main/java/com/komia/kmsys/controller/ErrorController.java
+++ src/main/java/com/komia/kmsys/controller/ErrorController.java
@@ -1,12 +1,9 @@
 package com.komia.kmsys.controller;
 
-import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
--- src/main/java/com/komia/kmsys/controller/LoginController.java
+++ src/main/java/com/komia/kmsys/controller/LoginController.java
@@ -1,18 +1,12 @@
 package com.komia.kmsys.controller;
 
-import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.AuthorizationException;
-import org.apache.shiro.session.InvalidSessionException;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
